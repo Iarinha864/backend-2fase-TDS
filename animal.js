43bf7aac391ae8b67ff1ec43b735921b44e2651d
Iarinha864 <iara.vitoria.ferreira@escola.pr.gov.br>
2026-06-03 13:53:18 +0000

    v1

--- animal.js
+++ animal.js
@@ -1,1 +1,9 @@
-
+const animal = {
+    nome: "Stella", //string
+    especie: "Cavalo", //string
+    idade:4, //number
+    tutor: false,
+    vacinado: true
+};
+console.log(animal);
+console.log(animal.nome);
