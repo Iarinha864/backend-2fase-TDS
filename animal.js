5f28b4de83213b2e7d3e49e4f667776b549f644b
Iarinha864 <iara.vitoria.ferreira@escola.pr.gov.br>
2026-06-08 13:52:05 +0000

    v1

--- animal.js
+++ animal.js
@@ -7,3 +7,9 @@ const animal = {
 };
 //console.log(animal);
 console.log(animal.nome);
+console.log(animal.tutor);
+
+animal.idade = 6;
+//console.log(animal);
+
+console.log(`${animal.nome} é um ${animal.especie} com a idade ${animal.idade} e possui sua vacina em dia (${animal.vacinado}).`);
